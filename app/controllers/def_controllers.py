8b03874926757cee71c7f8183248bd9ef6e6eb9e
Bruno Gouveia <brunovcg@gmail.com>
2021-08-25 18:45:29 -0300

    All functions maounted, still have to do the excpeption

--- app/controllers/def_controllers.py
+++ app/controllers/def_controllers.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+
 import pymongo
 from app.models.post_model import Post
 
@@ -33,6 +33,23 @@ def id_generator():
     return my_id
 
 
+def get_one_post(id):
+
+    post = db.posts.find_one({"id":id})
+
+    if not post:
+        return "Not found"
+
+    post.pop("_id")
+
+    return post
+
+
+def get_many_posts():
+    all_posts = list_posts()
+    return all_posts
+
+
 def add_post(*args):
 
     next_id = id_generator()
@@ -50,46 +67,44 @@ def del_post(id):
 
     id_list = [item["id"] for item in post_list]
 
+    
+
     if not id in id_list:
         return False
+    
+    post_to_delete = get_one_post(id)
 
     db.posts.delete_one({"id":id})
 
-    return True
+    return post_to_delete
 
-def upd_post(id, *args):
+
+def upd_post(package):
 
     post_list = list_posts()
 
-    time_now = datetime.utcnow()
+    
 
     id_list = [item["id"] for item in post_list]
 
-    if not id in id_list:
+    # print(f"AQUI2 >>>>>>> {package}")
+
+    if not package[0] in id_list:
         return False
 
-    db.posts.update_one({"id":id})
+    data = db.posts.find_one({"id": package[0]})
+    update_items = {"$set" : package[1]}
 
-    return True
+    db.posts.update_one(data, update_items)
+
+    updated_post = get_one_post(package[0])
+
+    return updated_post
 
 
 
-    return ...
 
-def get_one_post(id):
 
-    post = db.posts.find_one({"id":id})
-
-    if not post:
-        return "Not found"
-
-    post.pop("_id")
-
-    return post
-
-def get_many_posts():
-    all_posts = list_posts()
-    return all_posts
 
 
 
